e8bf04f0852669cea758d5a10aa3b8b5d3edba69
Ayon <ayonjodder177@gmail.com>
2022-02-10 19:18:12 +0600

    You are given an array prices where prices[i]

--- leetcode.js
+++ leetcode.js
@@ -16,3 +16,23 @@ function getTwoSum(nums, target) {
 console.log(getTwoSum(numsArray,target))
 
 
+
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
+
+function maxPro(prices) {
+    let profit = 0;
+    let min = prices[0];
+    
+    for(let i = 1; i < prices.length; ++i) { 
+        if(min > prices[i]) {
+            min = prices[i];
+        } else if(profit < (prices[i] - min)) {
+            profit = prices[i] - min;
+        }
+    }
+    return profit;
+};
+let pricesArray = [7,1,5,3,6,4];
+console.log(maxPro(pricesArray))
